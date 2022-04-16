4cc46a1928b4f4f6144120d1b24b75c4649b0698
Aaron Ragsdale <apragsdale@wisc.edu>
2022-04-15 20:24:28 -0500

    fill graph function

--- tests/load_dump.jl
+++ tests/load_dump.jl
@@ -119,25 +119,156 @@ function loadGraph(fname)
     return data
 end
 
+function getDemeInterval(deme, data)
+    if "start_time" in keys(deme)
+        start_time = deme["start_time"]
+    elseif "ancestors" in keys(deme)
+        anc = deme["ancestors"][1]
+        for deme2 in data["demes"]
+            if deme2["name"] == anc
+                start_time = deme2["epochs"][end]["end_time"]
+            end
+        end
+    else
+        start_time = Inf
+    end
+    if "end_time" in keys(deme["epochs"][end])
+        end_time = deme["epochs"][end]["end_time"]
+    else
+        end_time = 0
+    end
+    return [start_time, end_time]
+end
+
+function fillDeme(deme, deme_intervals, data)
+    full_deme = Dict{String, Any}()
+    full_deme["name"] = deme["name"]
+    if "description" in keys(deme)
+        full_deme["description"] = deme["description"]
+    else
+        full_deme["description"] = nothing
+    end
+    if "ancestors" in keys(deme)
+        full_deme["ancestors"] = deme["ancestors"]
+    else
+        full_deme["ancestors"] = []
+    end
+    if "proportions" in keys(deme)
+        full_deme["proportions"] = deme["proportions"]
+    elseif "ancestors" in keys(deme)
+        if length(deme["ancestors"]) == 1
+            full_deme["proportions"] = [1.0]
+        end
+    else
+        full_deme["proportions"] = []
+    end
+    if "start_time" in keys(deme)
+        full_deme["start_time"] = deme["start_time"]
+    elseif "ancestors" in keys(deme)
+        if length(deme["ancestors"]) == 1
+            full_deme["start_time"] = deme_intervals[deme["ancestors"][1]]
+        else
+            error("Too many ancestors!")
+        end
+    else
+        full_deme["start_time"] = Inf
+    end
+    full_deme["epochs"] = Dict{String, Any}[]
+    for epoch in deme["epochs"]
+        full_epoch = Dict{String, Any}()
+        # end time
+        if "end_time" in keys(epoch)
+            full_epoch["end_time"] = epoch["end_time"]
+        else
+            full_epoch["end_time"] = 0
+        end
+        # start size
+        if "start_size" in keys(epoch)
+            full_epoch["start_size"] = epoch["start_size"]
+        else
+            full_epoch["start_size"] = full_deme["epochs"][end]["end_time"]
+        end
+        # end size
+        if "end_size" in keys(epoch)
+            full_epoch["end_size"] = epoch["end_size"]
+        else
+            full_epoch["end_size"] = full_epoch["start_size"]
+        end
+        # size function
+        if "size_function" in keys(epoch)
+            full_epoch["size_function"] = epoch["size_function"]
+        elseif full_epoch["start_size"] == full_epoch["end_size"]
+            full_epoch["size_function"] = "constant"
+        else
+            full_epoch["size_function"] = "exponential"
+        end
+        # selfing rate
+        if "selfing_rate" in keys(epoch)
+            full_epoch["selfing_rate"] = epoch["selfing_rate"]
+        else
+            full_epoch["selfing_rate"] = 0
+        end
+        # cloning rate
+        if "cloning_rate" in keys(epoch)
+            full_epoch["cloning_rate"] = epoch["cloning_rate"]
+        else
+            full_epoch["cloning_rate"] = 0
+        end
+        push!(full_deme["epochs"], full_epoch)
+    end
+    return full_deme
+end
+
 function fillGraph(data::Dict)
-    if "description" ∉ keys(data)
-        data["description"] = nothing
-    end
-    if "doi" ∉ keys(data)
-        data["doi"] = []
-    end
-    if "migrations" ∉ keys(data)
-        data["migrations"] = []
-    end
-    if "pulses" ∉ keys(data)
-        data["pulses"] = []
-    end
-    if "generation_time" ∉ keys(data)
-        data["generation_time"] = nothing
-    end
+    validateGraph(data)
+    filled = Dict{String, Any}()
+    # add defaults
     if "defaults" ∉ keys(data)
-        data["defaults"] = Dict()
+        filled["defaults"] = Dict{String, Any}
+    else
+        filled["defaults"] = data["defaults"]
     end
+    # add demes
+    deme_intervals = Dict{String, Array}()
+    filled["demes"] = Dict{String, Any}[]
+    for deme in data["demes"]
+        deme_intervals[deme["name"]] = getDemeInterval(deme, data)
+        full_deme = fillDeme(deme, deme_intervals, data)
+        push!(filled["demes"], full_deme)
+    end
+    # add description
+    if "description" ∉ keys(data)
+        filled["description"] = nothing
+    else
+        filled["description"] = data["description"]
+    end
+    # add doi
+    if "doi" ∉ keys(data)
+        filled["doi"] = String[]
+    else
+        filled["doi"] = data["doi"]
+    end
+    # add generation time
+    if "generation_time" ∉ keys(data)
+        filled["generation_time"] = nothing
+    else
+        filled["generation_time"] = data["generation_time"]
+    end
+    # add migrations
+    if "migrations" ∉ keys(data)
+        filled["migrations"] = Dict{String, Any}[]
+    else
+        filled["migrations"] = data["migrations"]
+    end
+    # add pulses
+    if "pulses" ∉ keys(data)
+        filled["pulses"] = Dict{String, Any}[]
+    else
+        filled["pulses"] = data["pulses"]
+    end
+    # add time units
+    filled["time_units"] = data["time_units"]
+    return filled
 end
 
 function simplifyGraph(data::Dict)
