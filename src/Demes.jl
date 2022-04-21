184a05db09a14cd4d446a6e16da3ddd6dd671101
Aaron Ragsdale <apragsdale@wisc.edu>
2022-04-21 17:15:11 -0500

    start on simplifier

--- src/Demes.jl
+++ src/Demes.jl
@@ -10,6 +10,7 @@ include("utils.jl") # functions to manipulate and get information from model
 include("loader.jl") # read YAML formatted models as data
 include("builder.jl") # iteratively build demographic models
 include("validator.jl") # validate input data
-# include("writer.jl") # write models to YAML format
-# include("simplifier.jl") # simplify a model to non-redundant
+include("writer.jl") # write models to YAML format
+include("simplifier.jl") # simplify a model to non-redundant
+
 end
