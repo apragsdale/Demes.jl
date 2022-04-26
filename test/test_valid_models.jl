e81e702433bff2c8f63a0db1af320d41c7985a59
Aaron Ragsdale <apragsdale@wisc.edu>
2022-04-25 19:58:18 -0500

    manual test cases

--- test/test_valid_models.jl
+++ test/test_valid_models.jl
@@ -6,16 +6,15 @@ function loadModel(fname::String)
     return simplified
 end
 
-#@testset "ValidModels" begin
-#    for f in readdir(joinpath(@__DIR__, "../demes-spec/test-cases/valid"))
-#        fname = joinpath(@__DIR__, "../demes-spec/test-cases/valid", f)
-#        if f in ["pulse_edge_case_02.yaml"]
-#            # these might not be valide test cases...
-#            @test_skip typeof(loadModel(fname)) == Dict{Any,Any}
-#        else
-#            @test typeof(loadModel(fname)) == Dict{Any,Any}
-#        end
-#    end
-#end
-
-@test typeof(loadModel("gutenkunst_ooa.yaml")) == Dict{Any,Any}
+@testset "ValidModels" begin
+    for f in readdir(joinpath(@__DIR__, "test-cases/valid"))
+        fname = joinpath(@__DIR__, "test-cases/valid", f)
+        if f in ["pulse_edge_case_02.yaml"]
+            # these might not be valide test cases...
+            @test_skip typeof(loadModel(fname)) == Dict{Any,Any}
+        else
+            @test typeof(loadModel(fname)) == Dict{Any,Any}
+        end
+    end
+    @test typeof(loadModel("gutenkunst_ooa.yaml")) == Dict{Any,Any}
+end
